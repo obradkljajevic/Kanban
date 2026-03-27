1625af36fd1a81f205da3064bc7ea885921d3adc
Obrad <tvojemail@gmail.com>
2026-03-27 16:29:03 +0100

    Tabele i edit

---
+++ kanban.js
@@ -0,0 +1,102 @@
+const addButton = document.getElementById("add");
+const newTask = document.getElementById("newTask");
+const toDoList = document.getElementById("toDoList");
+
+addButton.addEventListener("click", () => {
+    const task = newTask.value.trim();
+    if (!task) return;
+
+    const newCard = document.createElement("div");
+    newCard.classList.add("card");
+    newCard.setAttribute('draggable', true);
+
+    const textSpan = document.createElement("span");
+    textSpan.classList.add("taskText");
+    textSpan.textContent = task;
+
+    const deleteBtn = document.createElement("button");
+    const doneBtn = document.createElement("button");
+    const editBtn = document.createElement("button");
+
+    const buttons = document.createElement("div");
+
+    editBtn.textContent = "✏️";
+    deleteBtn.textContent = "❌";
+    doneBtn.textContent = "✔️";
+
+    buttons.appendChild(editBtn);
+    buttons.appendChild(doneBtn);
+    buttons.appendChild(deleteBtn);
+
+    doneBtn.classList.add("doneBtn");
+    deleteBtn.classList.add("deleteBtn");
+    editBtn.classList.add("editBtn");
+    
+    deleteBtn.addEventListener("click", () => {
+        newCard.remove();
+    });
+
+    editBtn.addEventListener("click",()=>{
+
+        const textSpan = newCard.querySelector(".taskText");
+        const input = document.createElement("input");
+        input.type = "text";
+        input.value = textSpan.textContent;
+        newCard.replaceChild(input, textSpan);
+        input.focus();
+        input.classList.add("changeName");
+
+        input.addEventListener("keypress", (e) => {
+            if (e.key === "Enter") {
+                const newSpan = document.createElement("span");
+                newSpan.classList.add("taskText");
+                newSpan.textContent = input.value;
+                newCard.replaceChild(newSpan, input);
+             }
+        });
+
+    })
+    newCard.appendChild(textSpan);
+    newCard.appendChild(buttons);
+    toDoList.appendChild(newCard);
+
+    newTask.value = "";
+
+    newCard.addEventListener('dragstart', () => newCard.classList.add('dragging'));
+    newCard.addEventListener('dragend', () => newCard.classList.remove('dragging'));
+});
+
+const restart = document.getElementById("restart");
+const tasks = document.querySelectorAll(".tasks");
+
+restart.addEventListener("click",()=>{
+    tasks.forEach(t =>{
+        t.textContent="";
+    });
+});
+
+const h2 = document.querySelectorAll(".h2");
+
+h2.forEach(h => {
+    const editBtnH = document.createElement("button");
+    editBtnH.textContent = "✏️";
+    editBtnH.classList.add("editBtn");
+    h.appendChild(editBtnH);
+
+    editBtnH.addEventListener("click", () => {
+        const input2 = document.createElement("input");
+        input2.type = "text";
+        input2.value = h.textContent; 
+        h.textContent = "";
+        h.appendChild(input2);
+        input2.focus();
+        input2.classList.add("changeName");
+
+        input2.addEventListener("keypress", (e) => {
+            if (e.key === "Enter") {
+                h.textContent = input2.value;
+                h.appendChild(editBtnH);
+            }
+        });
+    });
+});
